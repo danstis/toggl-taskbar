25ee3b2e0ddb300c21df2ea10da719af6327d665
Dan Anstis <dan@bsod.co.nz>
2019-02-11 16:40:12 +1000

    Get open time entries

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"image/color"
 	"log"
-	"os"
 	"time"
 
 	"github.com/BurntSushi/toml"
@@ -60,10 +59,10 @@ func onReady() {
 		if err != nil {
 			log.Printf("Failed to get Toggl details: %v\n", err)
 		}
-		log.Printf("- Got new time %v:%v\n", t.hours, t.minutes)
+		log.Printf("- Got new time %d:%2d\n", t.hours, t.minutes) // TODO: remove this when logging goes away
 		updateIcon(fmt.Sprintf("%v", t.hours))
-		systray.SetTooltip(fmt.Sprintf("Toggl time tracker: %v:%v", t.hours, t.minutes))
-		time.Sleep(5 * time.Minute)
+		systray.SetTooltip(fmt.Sprintf("Toggl time tracker: %d:%2d", t.hours, t.minutes))
+		time.Sleep(1 * time.Minute)
 	}
 }
 
@@ -72,12 +71,27 @@ func onExit() {
 }
 
 func getWeeklyTime(c *Settings) (togglTime, error) {
+	closedTime, err := getClosedTimeEntries(c)
+	if err != nil {
+		return togglTime{}, fmt.Errorf("failed to get closed time entries: %v", err)
+	}
+	openTime, err := getOpenTimeEntry(c)
+	if err != nil {
+		return togglTime{}, fmt.Errorf("failed to get open time entry: %v", err)
+	}
+
+	return togglTime{
+		hours:   getHours(closedTime + openTime),
+		minutes: getMinutes(closedTime + openTime),
+	}, nil
+}
+
+func getClosedTimeEntries(c *Settings) (time.Duration, error) {
 	type WeeklyResponse struct {
 		TotalGrand int `json:"total_grand"`
 	}
 	var ct WeeklyResponse
 
-	// toggl := resty.New().SetHostURL("https://www.toggl.com/api/v8").SetBasicAuth(c.Token, "api_token")
 	toggleReports := resty.New().SetHostURL("https://toggl.com/reports/api/v2").SetBasicAuth(c.Token, "api_token")
 
 	_, err := toggleReports.R().
@@ -90,15 +104,43 @@ func getWeeklyTime(c *Settings) (togglTime, error) {
 		SetResult(&ct).
 		Get("/weekly")
 	if err != nil {
-		return togglTime{}, fmt.Errorf("unable to get summary report from the Toggl API: %v", err)
+		return time.Duration(0), fmt.Errorf("unable to get summary report from the Toggl API: %v", err)
 	}
 
-	closedTime := time.Duration(ct.TotalGrand) * time.Millisecond
+	return time.Duration(ct.TotalGrand) * time.Millisecond, nil
+}
 
-	return togglTime{
-		hours:   getHours(closedTime),
-		minutes: getMinutes(closedTime),
-	}, nil
+func getOpenTimeEntry(c *Settings) (time.Duration, error) {
+	type TimeEntriesResponse struct {
+		Data struct {
+			Duration int32 `json:"duration"`
+		} `json:"data"`
+	}
+	var ot TimeEntriesResponse
+
+	toggl := resty.New().SetHostURL("https://www.toggl.com/api/v8").SetBasicAuth(c.Token, "api_token")
+
+	_, err := toggl.R().
+		SetQueryParams(map[string]string{
+			"user_agent": c.Email,
+			"wid":        c.WorkspaceID,
+		}).
+		SetResult(&ot).
+		Get("/time_entries/current")
+	if err != nil {
+		return time.Duration(0), fmt.Errorf("unable to get current time entry from the Toggl API: %v", err)
+	}
+
+	// if the returned duration is not negative then there is no open entry.
+	if ot.Data.Duration >= 0 {
+		return 0, nil
+	}
+
+	// Calculate the number of seconds based on the input data.
+	// Unix epoc plus returned value of duration = seconds the current entry has been running for.
+	od := int32(time.Now().Unix()) + ot.Data.Duration
+
+	return time.Duration(od) * time.Second, nil
 }
 
 func getLastMonday() string {
@@ -147,10 +189,6 @@ func createIcon(x, y int, label string) ([]byte, error) {
 		return []byte{}, err
 	}
 	img := buf.Bytes()
-
-	icoimg, _ := os.Create("new.ico")
-	defer icoimg.Close()
-	_ = ico.Encode(icoimg, dc.Image())
 
 	return []byte(img), nil
 }
