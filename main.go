d1c5cc81ac9720e3f799e66927f36e3f81e4c5a5
Dan Anstis <dan@bsod.co.nz>
2019-02-11 11:18:11 +1000

    Add toggle weekly summary

--- main.go
+++ main.go
@@ -8,17 +8,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/BurntSushi/toml"
 	ico "github.com/Kodeworks/golang-image-ico"
 	"github.com/fogleman/gg"
 	"github.com/getlantern/systray"
+	"gopkg.in/resty.v1"
 )
 
 // Version contains the package version
 var Version = "0.0.0-default"
 
+const configFile = "config.toml"
+
 type togglTime struct {
 	hours   int
 	minutes int
+}
+
+// Settings contains the application configuration
+type Settings struct {
+	Token       string `toml:"token"`
+	UserID      string `toml:"userId"`
+	WorkspaceID string `toml:"workspaceId"`
+	Email       string `toml:"email"`
 }
 
 // Main entry point for the app.
@@ -28,6 +40,11 @@ func main() {
 
 func onReady() {
 	var t togglTime
+	var config Settings
+	_, err := toml.DecodeFile(configFile, &config)
+	if err != nil {
+		log.Fatalf("Failed to read configuration file: %v", err)
+	}
 	updateIcon("00")
 	mQuit := systray.AddMenuItem("Quit", "Quit the app")
 	go func() {
@@ -39,7 +56,10 @@ func onReady() {
 	systray.SetTitle("Toggl Weekly Time")
 
 	for {
-		t = getWeeklyTime()
+		t, err = getWeeklyTime(&config)
+		if err != nil {
+			log.Printf("Failed to get Toggl details: %v\n", err)
+		}
 		log.Printf("- Got new time %v:%v\n", t.hours, t.minutes)
 		updateIcon(fmt.Sprintf("%v", t.hours))
 		systray.SetTooltip(fmt.Sprintf("Toggl time tracker: %v:%v", t.hours, t.minutes))
@@ -51,11 +71,54 @@ func onExit() {
 	// Cleaning stuff here.
 }
 
-func getWeeklyTime() togglTime {
-	return togglTime{
-		hours:   0,
-		minutes: 0,
+func getWeeklyTime(c *Settings) (togglTime, error) {
+	type WeeklyResponse struct {
+		TotalGrand int `json:"total_grand"`
 	}
+	var ct WeeklyResponse
+
+	// toggl := resty.New().SetHostURL("https://www.toggl.com/api/v8").SetBasicAuth(c.Token, "api_token")
+	toggleReports := resty.New().SetHostURL("https://toggl.com/reports/api/v2").SetBasicAuth(c.Token, "api_token")
+
+	_, err := toggleReports.R().
+		SetQueryParams(map[string]string{
+			"user_agent":   c.Email,
+			"workspace_id": c.WorkspaceID,
+			"user_ids":     c.UserID,
+			"since":        getLastMonday(),
+		}).
+		SetResult(&ct).
+		Get("/weekly")
+	if err != nil {
+		return togglTime{}, fmt.Errorf("unable to get summary report from the Toggl API: %v", err)
+	}
+
+	closedTime := time.Duration(ct.TotalGrand) * time.Millisecond
+
+	return togglTime{
+		hours:   getHours(closedTime),
+		minutes: getMinutes(closedTime),
+	}, nil
+}
+
+func getLastMonday() string {
+	t := time.Now()
+	delta := (int(t.Weekday()) - 1) * -1
+	t = t.AddDate(0, 0, delta)
+
+	return t.Format("2006-01-02")
+}
+
+func getHours(t time.Duration) int {
+	d := t.Round(time.Minute)
+	h := d / time.Hour
+	return int(h)
+}
+
+func getMinutes(t time.Duration) int {
+	d := t.Round(time.Minute) % time.Hour
+	m := d / time.Minute
+	return int(m)
 }
 
 func updateIcon(hours string) {
