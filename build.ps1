05819b16274d0d35d8dc55b758efa723933a0e0e
Dan Anstis <dan@bsod.co.nz>
2019-02-11 18:47:54 +1000

    Add icon to windows exe +semver: minor (#12)
    Closes #5
    +semver: Minor

--- build.ps1
+++ build.ps1
@@ -1,1 +1,3 @@
-go build -ldflags "-H=windowsgui -X main.Version=$(gitversion /output json /showvariable SemVer)"
+$Version = GitVersion.exe | ConvertFrom-Json
+goversioninfo -64 -product-version="$($Version.SemVer)" -ver-major="$($Version.Major)" -ver-minor="$($Version.Minor)" -ver-patch="$($Version.Patch)" -product-ver-major="$($Version.Major)" -product-ver-minor="$($Version.Minor)" -product-ver-patch="$($Version.Patch)"
+go build -ldflags "-H=windowsgui -X main.Version=$($Version.SemVer)"
