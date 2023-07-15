5f8121d506f5d117912d1e8a6e80616990f1bc0e
Dan Anstis <dan@bsod.co.nz>
2023-07-15 13:17:09 +1000

    fix: update flags in goreleaser config

--- .build.ps1
+++ .build.ps1
@@ -1,3 +1,3 @@
 $Version = GitVersion.exe | ConvertFrom-Json
 goversioninfo -64 -product-version="$($Version.SemVer)" -ver-major="$($Version.Major)" -ver-minor="$($Version.Minor)" -ver-patch="$($Version.Patch)" -product-ver-major="$($Version.Major)" -product-ver-minor="$($Version.Minor)" -product-ver-patch="$($Version.Patch)"
-go build -ldflags "-H=windowsgui -X main.Version=$($Version.SemVer)"
+go build -ldflags "-H=windowsgui -X main.version=$($Version.SemVer)"
